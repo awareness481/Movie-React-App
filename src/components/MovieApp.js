e2fca9f3f941efadf3b6db8c3e7e8ee4092f8b34
Jesse Jafa <jesse.jafa@gmail.com>
2018-05-02 14:44:44 +0300

    Initial main app structure

--- src/components/MovieApp.js
+++ src/components/MovieApp.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import NewMovies from './NewMovies';
+import Action from './Action';
 
-class MovieApp extends React.Component {
-  constructor() {
+export default class MovieApp extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      movie: "",
+      movieSelected: false 
+    }
+
+    this.handleAction = this.handleAction.bind(this);
+  }
+
+  handleAction(e) {
+    this.setState({ 
+      movieSelected: true,
+      movie: e
+    });
+  }
+  componentDidUpdate() {
+    console.log('update!');
   }
 
   render() {
     return (
       <div>
-        <form>
-          <input type="text" />
-        </form>
-        <NewMovies />
+        <Action
+          onHandleAction={this.handleAction} 
+        />
       </div>
-    )
+    );
   }
 }
