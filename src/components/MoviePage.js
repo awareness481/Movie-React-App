b77d8c024c0dcd476ef1d75ed4082c9cbf1012e9
Jesse Jafa <jesse.jafa@gmail.com>
2018-05-15 21:19:51 +0300

    Initial MoviePage

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Movie extends React.Component {
+export default class MoviePage extends React.Component {
   constructor(props) {
     super(props);
 
@@ -27,12 +27,12 @@ export default class Movie extends React.Component {
   }
 
   componentDidMount(props) {
-    const id = this.props.match.params.id;
-    const url = `http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`;
+    const id = this.props.movie;
+    const url = `http://www.omdbapi.com/?t=${id}&plot=full&apikey=API_KEY`;
 
     fetch(url)
-    .then(res => res.json())
-    .then(data => {this.handleMovieDetails(data)}
+      .then(res => res.json())
+      .then(data => {this.handleMovieDetails(data)}
     );
   }
 
@@ -48,24 +48,28 @@ export default class Movie extends React.Component {
       Actors: movie.Actors,
       Awards: movie.Awards,
       Plot: movie.Plot,
+      Poster: movie.Poster
     })
   }
 
   render() {
     return (
-      <div>
-        <h1>{this.state.Title}</h1>
-        <ul>
-          <li>{this.state.Year}</li>
-          <li>{this.state.Rating}</li>
-          <li>{this.state.Runtime}</li>
-          <li>{this.state.Genre}</li>
-          <li>{this.state.Directo}r</li>
-          <li>{this.state.Writer}</li>
-          <li>{this.state.Actors}</li>
-          <li>{this.state.Awards}</li>
-        </ul>
-        <p>{this.state.Plot}</p>
+      <div className='movie'>
+        <div className='movie-container'>
+          <div className='movie-poster'>
+            <img src={this.state.Poster} />
+          </div>
+          <div className='movie-two'>
+            <h1 className='movie-title'>{this.state.Title}</h1>
+            <span className='subtitle'>{this.state.Rating}, {this.state.Runtime}</span>
+            <p>{this.state.Plot}</p>
+            <div className='movie-details'>
+              <span><h3>Original Release: </h3>{this.state.Year}</span>
+              <span><h3>Genre: </h3>{this.state.Genre}</span>
+              <span><h3>Writer: </h3>{this.state.Writer}</span><span><h3>Director: </h3>{this.state.Director}</span>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
