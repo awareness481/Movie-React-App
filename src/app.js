d30688587b57add51206fa16ec4c3717002ac52c
Jesse Jafa <jesse.jafa@gmail.com>
2018-05-16 18:28:09 +0300

    Removed react-router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
-
-import AppRouter from './routers/AppRouter';
+import MovieApp from './components/MovieApp';
 import './styles/styles.scss';
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(<MovieApp />, document.getElementById('app'));
